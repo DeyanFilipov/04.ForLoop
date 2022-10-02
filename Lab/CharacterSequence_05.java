6e7b8ef16596e9de266b9b7f4ec303b0b36dceb2
DeyanFilipov <didkofilipov.sv@gmail.com>
2022-10-02 11:14:31 +0300

    Commit

---
+++ Lab/CharacterSequence_05.java
@@ -0,0 +1,20 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class CharacterSequence_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        int lengthText = text.length();
+
+        for (int i = 0; i < lengthText; i++) {
+            char symbol = text.charAt(i);
+            System.out.println(symbol);
+        }
+
+
+    }
+}
